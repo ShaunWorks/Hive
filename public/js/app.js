887873678928079fc84052f86fb0bfad94939189
Torian <tharris@zyrobotics.com>
2019-03-05 13:19:48 -0500

    testing/vue input

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
-let app = new Vue({
-    el: "#app",
+let chatBox = new Vue({
+    el: "#chat-box",
     data: {
         message: "Nice Vue!"
     }
@@ -7,12 +7,17 @@ let app = new Vue({
 
 Vue.component("list-item", {
     props: ['name'],
-    template: '<button type="button" class="list-group-item list-group-item-action px-1">{{name}}</button>'
+    template: '<button v-on:click="sayHi" type="button" class="list-group-item list-group-item-action px-1">{{name}}</button>',
+    methods: {
+        sayHi: function () {
+            chatBox.message = this.name;
+        }
+    }
 })
 
 new Vue({
     el: '#hive-nav',
     data: {
         items: ["banana", "orange", "apple", "fdaff", "adfadf", "fdsfasf", "adfdafawf", "dfaf", "Fsdfasfd", "daffafd", "fadsffa", "fadfdf", "fadsfafdaf", "dVvvcvzv"]
-      }
+    }
 })

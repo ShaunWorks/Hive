b3980dcaf56b8d63b9bcfc4fe976fea06458ecb1
Torian <tharris@zyrobotics.com>
2019-03-05 11:28:15 -0500

    sidebar changes

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,15 @@ let app = new Vue({
         message: "Nice Vue!"
     }
 })
+
+Vue.component("list-item", {
+    props: ['name'],
+    template: '<button type="button" class="list-group-item list-group-item-action px-1">{{name}}</button>'
+})
+
+new Vue({
+    el: '#hive-nav',
+    data: {
+        items: ["banana", "orange", "apple", "fdaff", "adfadf", "fdsfasf", "adfdafawf", "dfaf", "Fsdfasfd", "daffafd", "fadsffa", "fadfdf", "fadsfafdaf", "dVvvcvzv"]
+      }
+})
